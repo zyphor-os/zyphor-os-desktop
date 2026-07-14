40d72ee7eef55c8dafa2c8ebda105e4f439eb5a2
markjasonespelita <markjasonespelita02@gmail.com>
2026-07-14 22:45:01 +0800

    feat: added zyphor command center profile module

---
+++ pkg/v2/zcc/usr/share/zyphor-command-center/ui/profile/profile.py
@@ -0,0 +1,16 @@
+from PyQt6.QtWebEngineWidgets import QWebEngineView
+from PyQt6.QtCore import QUrl
+
+from . import server
+
+
+class ProfilePage(QWebEngineView):
+
+    def __init__(self):
+        super().__init__()
+
+        # Start Laravel if needed
+        server.start()
+
+        # Load the Laravel page
+        self.load(QUrl("http://127.0.0.1:2437"))
