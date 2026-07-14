eddb56e38fb23d5c79ede05361938e674ed737b9
markjasonespelita <markjasonespelita02@gmail.com>
2026-07-14 22:45:01 +0800

    feat: updated zyphor command center main entry

--- pkg/v2/zcc/usr/share/zyphor-command-center/main.py
+++ pkg/v2/zcc/usr/share/zyphor-command-center/main.py
@@ -1,9 +1,16 @@
 from app import MainWindow
 from PyQt6.QtWidgets import QApplication
+from ui.profile import server
 import sys
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+
     window = MainWindow()
     window.show()
-    sys.exit(app.exec())
+
+    exit_code = app.exec()
+
+    server.stop()
+
+    sys.exit(exit_code)
