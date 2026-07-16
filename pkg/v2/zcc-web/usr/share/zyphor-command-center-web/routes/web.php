a3ee790f26b8a9cd8fe3332b1b9f0f6c4d8c89cc
markjasonespelita <markjasonespelita02@gmail.com>
2026-07-16 10:40:53 +0800

    chore: updated command center web routes

--- pkg/v2/zcc-web/usr/share/zyphor-command-center-web/routes/web.php
+++ pkg/v2/zcc-web/usr/share/zyphor-command-center-web/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
-Route::get('/profile', function () {
-    return view('profile');
-});
+// View
+Route::get('/profile', [ProfileController::class, 'getUserName']);
+
+// APIs
+Route::get('/get-user-info', [ProfileController::class, 'getUserInfo']);
+Route::post('/profile/update', [ProfileController::class, 'updateProfile']);
+Route::post('/profile/avatar', [ProfileController::class, 'uploadAvatar']);
+Route::delete('/profile/avatar', [ProfileController::class, 'removeAvatar']);
