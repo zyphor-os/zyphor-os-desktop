dbac325b5d1bdc85975024d7f550caab1d0e968a
markjasonespelita <markjasonespelita02@gmail.com>
2026-09-08 10:19:35 +0800

    feat: update zysh prompt

--- pkg/v2/zysh/usr/share/zysh/prompt.zsh
+++ pkg/v2/zysh/usr/share/zysh/prompt.zsh
@@ -28,7 +28,7 @@ configure_prompt() {
 configure_prompt
 
 OS_NAME="$(grep '^PRETTY_NAME=' /etc/os-release | cut -d= -f2- | tr -d '"')"
-BUILD="2026.09.05-r47"
+BUILD="2026.09.08-r48"
 
 echo ""
 figlet -f term "$OS_NAME" | lolcat
