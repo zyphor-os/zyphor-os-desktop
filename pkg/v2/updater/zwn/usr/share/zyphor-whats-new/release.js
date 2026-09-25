56e235d64c1bf77759958fd42ae7508b051ef5d2
markjasonespelita <markjasonespelita02@gmail.com>
2026-09-25 15:32:04 +0800

    chore: update What's New release data

--- pkg/v2/updater/zwn/usr/share/zyphor-whats-new/release.js
+++ pkg/v2/updater/zwn/usr/share/zyphor-whats-new/release.js
@@ -1,7 +1,7 @@
 const releaseNotes = {
     distro: "Zyphor OS Ada Lovelace LTS",
-    version: "2.2.1-ada-lovelace-lts-u61",
-    date: "Updated On: September 24, 2026 @ 10:00 AM",
+    version: "2.2.1-ada-lovelace-lts-u62",
+    date: "Updated On: September 25, 2026 @ 03:28 PM",
 
     sections: [
         {
